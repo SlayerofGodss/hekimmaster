5abe86ea6c1865ae8f3a8fde284e087ecfa06871
diyar <diyarbjk44@gmail.com>
2021-01-23 00:49:19 +0300

    calender

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { from, Subscription } from 'rxjs';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import momentPlugin from '@fullcalendar/moment';
 
+
+
+
+
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { AccountService } from 'app/core/auth/account.service';
+import { EventService } from 'app/home/event.service';
 import { Account } from 'app/core/user/account.model';
 import { toMoment, toDuration } from '@fullcalendar/moment';
 import * as moment from 'moment';
 
+export class CalendarEvent {
+  title: any = '';
+  start: any;
+  end: any;
+
+
+  constructor() {}
+}
 
 @Component({
   selector: 'jhi-home',
@@ -21,22 +34,33 @@ export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
   authSubscription?: Subscription;
 
-  options: any;
   events: any[];
 
+  options: any;
 
-   constructor(private accountService: AccountService, private loginModalService: LoginModalService) {
-   this.events = [];
+  header: any;
+
+   constructor(private eventService: EventService,private accountService: AccountService, private loginModalService: LoginModalService) {
+    this.events = [];
+        this.options = {};
+        this.header = {};
+
+        
   }
+
+  
 
   ngOnInit(): void {
     this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
+    this.eventService.getEvents().subscribe(events => {this.events = events;});
+
 
     this.options = {
           plugins: [momentPlugin, dayGridPlugin, timeGridPlugin, interactionPlugin],
           titleFormat: 'MMMM D, YYYY',
           defaultDate: '2020-10-06',
           locale: 'tr',
+          selectable: true,
           themeSystem: 'litera',
           droppable: true,
           buttonText: {
